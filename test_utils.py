0ce9cad2d9746e1e01226d1db9440e52c13b12d6
Eric Ma <ericmajinglong@gmail.com>
2019-12-21 15:55:51 -0500

    refactored out some functions, added tests

---
+++ test_utils.py
@@ -0,0 +1,27 @@
+from utils import featurize_sequence_, molecular_weights, predict
+import pytest
+from hypothesis import given, strategies as st
+import joblib
+import gzip
+import pickle as pkl
+
+@given(
+    st.text(alphabet=list(molecular_weights.keys()), min_size=0, max_size=200)
+)
+def test_featurize_sequence_(sequence):
+    feats = featurize_sequence_(sequence)
+    assert feats.shape[-1] == len(sequence)
+
+    
+@given(sequence=st.text(alphabet=list(molecular_weights.keys()), min_size=99, max_size=99))
+def test_predict(sequence):
+    """
+    Baseline test that under ideal situations, 
+    i.e. len(sequence) == 99 and 
+    set(sequence).issubset(molecular_weights.keys()),
+    that the function executes correctly.
+    """
+    with gzip.open("data/models/ATV.pkl.gz", "rb") as f:
+        model = pkl.load(f)
+        
+    preds = predict(model, sequence)
--- utils.py
+++ utils.py
@@ -1,33 +1,79 @@
 import numpy as np
 
 molecular_weights = {
-    'A':89.0935,
-    'R':174.2017,
-    'N':132.1184,
-    'D':133.1032,
-    'C':121.1590,
-    'E':147.1299,
-    'Q':146.1451,
-    'G':75.0669,
-    'H':155.1552,
-    'I':131.1736,
-    'L':131.1736,
-    'K':146.1882,
-    'M':149.2124,
-    'F':165.1900,
-    'P':115.1310,
-    'S':105.0930,
-    'T':119.1197,
-    'W':204.2262,
-    'Y':181.1894,
-    'V':117.1469,
-    'X':100.00
+    "A": 89.0935,
+    "R": 174.2017,
+    "N": 132.1184,
+    "D": 133.1032,
+    "C": 121.1590,
+    "E": 147.1299,
+    "Q": 146.1451,
+    "G": 75.0669,
+    "H": 155.1552,
+    "I": 131.1736,
+    "L": 131.1736,
+    "K": 146.1882,
+    "M": 149.2124,
+    "F": 165.1900,
+    "P": 115.1310,
+    "S": 105.0930,
+    "T": 119.1197,
+    "W": 204.2262,
+    "Y": 181.1894,
+    "V": 117.1469,
+    "X": 100.00,
 }
 
 
-def featurize_sequence_(x):
-    """x: a string in a pandas DataFrame cell"""
+def featurize_sequence_(x, expected_size=99):
+    """
+    :param x: a string in a pandas DataFrame cell
+    """
     feats = np.zeros(len(x))
     for i, letter in enumerate(x):
         feats[i] = molecular_weights[letter]
-    return feats
+    return feats.reshape(1, -1)
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+
+
+def fit_model(data, features, target):
+    import janitor
+    model = RandomForestRegressor(n_estimators=300)
+    
+    resistance_data = features.join(data[target]).dropna()
+    X, y = resistance_data.get_features_targets(target_column_names=target)
+    
+    model.fit(X, y)
+    return model
+
+
+def cross_validate(data, features, target):
+    import janitor
+    model = RandomForestRegressor(n_estimators=500)
+    
+    resistance_data = features.join(data[target]).dropna()
+    X, y = resistance_data.get_features_targets(target_column_names=target)
+    
+    return -cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+
+
+class SequenceError(Exception):
+    pass
+
+def predict(model, sequence):
+    """
+    :param model: sklearn model
+    :param sequence: A string, should be 99 characters long.
+    """
+    if len(sequence) != 99:
+        raise ValueError(f"sequence must be of length 99. Your sequence is of length {len(sequence)}")
+    
+    if not set(sequence).issubset(set(molecular_weights.keys())):
+        invalid_chars = set(sequence).difference(molecular_weights.keys())
+        raise SequenceError(f"sequence contains invalid characters: {invalid_chars}")
+    
+    seqfeat = featurize_sequence_(sequence)
+    return model.predict(seqfeat)
