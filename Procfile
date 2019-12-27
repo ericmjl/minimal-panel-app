a27709a8656f05f179654cc5cf54fce0b33d5e80
Eric Ma <ericmajinglong@gmail.com>
2019-12-27 07:46:01 -0500

    multi-source app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: panel serve --address="0.0.0.0" --port=$PORT minimal-panel.ipynb --allow-websocket-origin=minimal-panel.ericmjl.com
+web: panel serve --address="0.0.0.0" --port=$PORT minimal-panel.ipynb home.ipynb --allow-websocket-origin=minimal-panel.ericmjl.com
--- test_utils.py
+++ test_utils.py
@@ -12,16 +12,16 @@ def test_featurize_sequence_(sequence):
     feats = featurize_sequence_(sequence)
     assert feats.shape[-1] == len(sequence)
 
-    
+
 @given(sequence=st.text(alphabet=list(molecular_weights.keys()), min_size=99, max_size=99))
 def test_predict(sequence):
     """
-    Baseline test that under ideal situations, 
-    i.e. len(sequence) == 99 and 
+    Baseline test that under ideal situations,
+    i.e. len(sequence) == 99 and
     set(sequence).issubset(molecular_weights.keys()),
     that the function executes correctly.
     """
     with gzip.open("data/models/ATV.pkl.gz", "rb") as f:
         model = pkl.load(f)
-        
+
     preds = predict(model, sequence)
